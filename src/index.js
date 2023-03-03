bb86e49f8cf616c62168a4e23a6508c9192f4dc7
aboriskaa <aboriska87@gmail.com>
2023-03-04 02:40:34 +0300

    part2

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import('./main.scss');
+
+import { drawGalleryItem } from './item';
+
+import items from './items';
+
+const galleryRootElement = document.getElementById('gallery');
+
+items.map((item) => galleryRootElement.appendChild(drawGalleryItem(item)));
+
 console.log('init');
---
+++ src/items.js
@@ -0,0 +1,23 @@
+import image1 from '../media/images/1.jpg';
+import image2 from '../media/images/2.jpg';
+import image3 from '../media/images/3.jpg';
+import image4 from '../media/images/4.jpg';
+
+export default [
+	{
+		resource: image1,
+		title: 'Mill by the river',
+	},
+	{
+		resource: image2,
+		title: 'Mountains in autumn',
+	},
+	{
+		resource: image3,
+		title: 'Trees in summer',
+	},
+	{
+		resource: image4,
+		title: 'Animated cat',
+	},
+];
